a7a394bef65afcc80c85841f2b5a4b7f11583ca5
Jason Curran <jc@jasoncurran.com>
2014-01-24 10:36:48 -0500

    Just a test

--- delete-repo.sh
+++ delete-repo.sh
@@ -8,3 +8,6 @@ curl -u jasoncurran215 https://api.github.com/authorizations
 
 # Now we can programatically delete repositories with our OAuth token.
 curl -X DELETE -H 'Authorization: token 754c997eb93fcae29a9ca1e7ea2932eab49c17f8' https://api.github.com/repos/jasoncurran215/REPOSITORYNAME
+
+
+#
