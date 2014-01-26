f871b88edfd768a520ce8ba0ffdee91fdd381b9d
Jason Curran <jc@jasoncurran.com>
2014-01-25 20:35:00 -0500

    Updated to reflect that second command is optional

--- delete-repo.sh
+++ delete-repo.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 
 #
-# NOTES: You must run the first two commands at the command line to get a proper OAuth token.
+# NOTES: You must run the first command at the command line to get a proper OAuth token.
 #
 
-# Create an OAuth token with multiple scopes. Enter password at prompt.
-# curl -u jasoncurran215 -X POST https://api.github.com/authorizations --data '{"scopes":["delete_repo", "user", "repo", "gist"]"}'
+# 1) Create an OAuth token with multiple scopes. Enter password at prompt.
+# curl -u USERNAME -X POST https://api.github.com/authorizations --data '{"scopes":["delete_repo", "user", "repo", "gist"]"}'
 
-# View OAuth tokens for this user. Enter password at prompt.
-# curl -u jasoncurran215 https://api.github.com/authorizations
+# 2) Optional: View OAuth tokens for this user. Enter password at prompt.
+# curl -u USERNAME https://api.github.com/authorizations
 
-# Now we can programatically delete repositories with our OAuth token.
-curl -X DELETE -H 'Authorization: token 00000000000000000000000000000000000000' https://api.github.com/repos/jasoncurran215/REPOSITORYNAME
+# 3) Now we can programatically delete repositories with our OAuth token.
+curl -X DELETE -H 'Authorization: token 00000000000000000000000000000000000000' https://api.github.com/repos/USERNAME/REPOSITORYNAME
